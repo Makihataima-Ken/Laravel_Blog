a5f7164672380323b76641f603c635476ad2b816
Ken <ahmadsoud63@gmail.com>
2024-09-25 01:10:37 +0300

    set up posts attributes

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[HomeController::class,'homepage']);
@@ -16,3 +17,5 @@ Route::middleware([
 });
 
 Route::get('/home',[HomeController::class,'index'])->name('home');
+Route::get('/post_page',[AdminController::class,'post_page']);
+Route::post('/add_post',[AdminController::class,'add_post']);
